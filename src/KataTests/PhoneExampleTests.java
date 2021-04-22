1a6f9555706570c5c6256633bf96313b52935d45
Daniel Neubieser <corataca@outlook.de>
2021-04-22 20:28:10 +0200

    CreatePhoneNumber

---
+++ src/KataTests/PhoneExampleTests.java
@@ -0,0 +1,13 @@
+package KataTests;
+
+import Kata.CreatePhoneNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PhoneExampleTests {
+    @Test
+    public void tests() {
+        assertEquals("(123) 456-7890", CreatePhoneNumber.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+}
